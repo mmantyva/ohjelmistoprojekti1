ff6dfa3c92173085b92bef80a6078e17a4456f47
Niina Kuusisto <niina.kuusisto@myy.haaga-helia.fi>
2021-02-11 17:17:31 +0200

    Tapahtuma repository

---
+++ ticketguru/src/main/java/swd4ta022/ticketguru/domain/TapahtumaRepository.java
@@ -0,0 +1,7 @@
+package swd4ta022.ticketguru.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TapahtumaRepository extends CrudRepository<Tapahtuma, Long> {
+
+}

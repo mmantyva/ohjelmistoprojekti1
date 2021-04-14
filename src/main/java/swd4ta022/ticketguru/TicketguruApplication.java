940215061b08ea7b051688c196a4b4a74bd204ac
mmantyva <60605429+mmantyva@users.noreply.github.com>
2021-04-14 20:01:12 +0300

    demodatalla

--- src/main/java/swd4ta022/ticketguru/TicketguruApplication.java
+++ src/main/java/swd4ta022/ticketguru/TicketguruApplication.java
@@ -34,27 +34,28 @@ public class TicketguruApplication {
 	 public CommandLineRunner demo(VentRepository ventrepository, VenueRepository vrepository, TypeRepository typerepository, TransactionRepository trrepository, TicketRepository ticketrepository, AppuserRepository arepository) {
 		 return (args)->{
 			 
-				/*
-				 * //demodataa, aja tämä vain kerran sen jälkeen, kun olet asentanut pysyvän
-				 * tietokannan. muuten tulee tuplia Venue venue1 = new Venue("Tavastia",
-				 * "Urhokekkosenkatu", "00100", "Helsinki"); Venue venue2 = new
-				 * Venue("Finlandiatalo", "Mansku", "00100", "Helsinki"); Venue venue3 = new
-				 * Venue("Lutakko", "Lutakonaukio 3", "40100", "Jyväskylä");
-				 * vrepository.save(venue1); vrepository.save(venue2); vrepository.save(venue3);
-				 * 
-				 * Vent vent1 = new Vent("Iso D", "11.11.2022", "Danny", "Kuvaus1", 2000,
-				 * vrepository.findByVenuename("Tavastia").get(0)); Vent vent2 = new
-				 * Vent("Konsertti", "22.1.2021. klo. 19", "Armi", "Kuvaus2", 2000,
-				 * vrepository.findByVenuename("Finlandiatalo").get(0));
-				 * ventrepository.save(vent1); ventrepository.save(vent2);
-				 * 
-				 * Type type1 = new Type("Child", 10.0); Type type2 = new Type("Adult", 30.0);
-				 * Type type3 = new Type("Student", 15.0); typerepository.save(type1);
-				 * typerepository.save(type2); typerepository.save(type3);
-				 * 
-				 * 
-				 * Transaction transaction1 = new Transaction("2021-03-03");
-				 * trrepository.save(transaction1); */
+			 Venue venue1 = new Venue("Tavastia", "Urhokekkosenkatu", "00100", "Helsinki");
+			 Venue venue2 = new Venue("Finlandiatalo", "Mansku", "00100", "Helsinki");
+			 Venue venue3 = new Venue("Lutakko", "Lutakonaukio 3", "40100", "Jyväskylä");
+			 vrepository.save(venue1);
+			 vrepository.save(venue2);
+			 vrepository.save(venue3);
+			 
+			 Vent event1 = new Vent("Iso D", "11.11.2022", "Danny", "Kuvaus1", 2000, vrepository.findByVenuename("Tavastia").get(0));
+			 Vent event2 = new Vent("Konsertti", "22.1.2021. klo. 19", "Armi", "Kuvaus2", 2000, vrepository.findByVenuename("Finlandiatalo").get(0));
+			 ventrepository.save(event1);
+			 ventrepository.save(event2);
+			 
+			 Type type1 = new Type("Child", 10.0);
+			 Type type2 = new Type("Adult", 30.0);
+			 Type type3 = new Type("Student", 15.0);
+			 typerepository.save(type1);
+			 typerepository.save(type2);
+			 typerepository.save(type3);
+			 
+			 
+			 Transaction transaction1 = new Transaction("2021-03-03");
+			 trrepository.save(transaction1);
 
 				 //User
 				 Appuser appuser = new Appuser("Tuomas", "Martsa", "user", "user",

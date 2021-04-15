5936f784d046700261a40fedb5a0311b85fa1c06
tuomas <mikko.martikainen3@myy.haaga-helia.fi>
2021-04-15 22:02:49 +0300

    cors test-3

--- src/main/java/swd4ta022/ticketguru/CorsConfiguration.java
+++ src/main/java/swd4ta022/ticketguru/CorsConfiguration.java
@@ -1,5 +1,6 @@
 package swd4ta022.ticketguru;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -9,10 +10,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class CorsConfiguration implements WebMvcConfigurer
 {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedMethods("GET", "POST");
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedMethods("GET", "POST");
+//    }
+    
+   @Bean
+    public WebMvcConfigurer corsConfigurer() 
+    {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
- 
 }

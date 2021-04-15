c74a5e61c0c8e1fa58c31bb90d708bdbb30200a9
tuomas <mikko.martikainen3@myy.haaga-helia.fi>
2021-04-15 21:51:13 +0300

    cors test

--- src/main/java/swd4ta022/ticketguru/web/TicketController.java
+++ src/main/java/swd4ta022/ticketguru/web/TicketController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import swd4ta022.ticketguru.domain.Ticket;
 import swd4ta022.ticketguru.domain.TicketRepository;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @Controller
 public class TicketController {
 	

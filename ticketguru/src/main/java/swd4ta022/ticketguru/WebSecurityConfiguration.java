7b226f0757160b4d57b936efa3f207f93e11c5ff
tuomas <mikko.martikainen3@myy.haaga-helia.fi>
2021-04-15 17:25:07 +0300

    disable auth

--- ticketguru/src/main/java/swd4ta022/ticketguru/WebSecurityConfiguration.java
+++ ticketguru/src/main/java/swd4ta022/ticketguru/WebSecurityConfiguration.java
@@ -1,36 +1,36 @@
-package swd4ta022.ticketguru;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.stereotype.Component;
-
-import swd4ta022.ticketguru.domain.Appuser;
-import swd4ta022.ticketguru.domain.DetailsService;
-
-@Component
-@EnableWebSecurity
-public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
-	
-	@Autowired
-	DetailsService detailsService;
-	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(detailsService).passwordEncoder(Appuser.PASSWORD_ENCODER);
-	}
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-			.anyRequest()
-			.authenticated()
-			.and()
-			.httpBasic()
-			.and()
-			.csrf()
-			.disable();
-	}
-}
+//package swd4ta022.ticketguru;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.stereotype.Component;
+//
+//import swd4ta022.ticketguru.domain.Appuser;
+//import swd4ta022.ticketguru.domain.DetailsService;
+//
+//@Component
+//@EnableWebSecurity
+//public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+//	
+//	@Autowired
+//	DetailsService detailsService;
+//	
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.userDetailsService(detailsService).passwordEncoder(Appuser.PASSWORD_ENCODER);
+//	}
+//	
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http.authorizeRequests()
+//			.anyRequest()
+//			.authenticated()
+//			.and()
+//			.httpBasic()
+//			.and()
+//			.csrf()
+//			.disable();
+//	}
+//}

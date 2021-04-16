2ab39f3c9bbc0e3a177560e34a1db7c853d84180
tuomas <mikko.martikainen3@myy.haaga-helia.fi>
2021-04-16 17:01:07 +0300

    cors-test6

--- src/main/java/swd4ta022/ticketguru/CorsConfiguration.java
+++ src/main/java/swd4ta022/ticketguru/CorsConfiguration.java
@@ -11,12 +11,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfiguration 
 {
    @Bean
-    public WebMvcConfigurer corsConfigurer() 
-    {
+    public WebMvcConfigurer corsConfigurer(){
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
+                registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowCredentials(false);
             }
         };
     }

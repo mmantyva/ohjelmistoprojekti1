57d8a793841b2d947af148a9ed9cf7c079d1ec2c
TuomasMartsa <68655596+TuomasMartsa@users.noreply.github.com>
2021-04-17 18:16:24 +0300

    CorsConfig pois

--- src/main/java/swd4ta022/ticketguru/CorsConfiguration.java
+++ src/main/java/swd4ta022/ticketguru/CorsConfiguration.java
@@ -1,4 +1,4 @@
-package swd4ta022.ticketguru;
+/* package swd4ta022.ticketguru;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -30,4 +30,4 @@ public class CorsConfiguration implements WebMvcConfigurer{
             }
         };
     }
-}
+} */

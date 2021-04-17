6fa34771ad2169f48600130690bcd4e094bd47c1
tuomas <mikko.martikainen3@myy.haaga-helia.fi>
2021-04-17 18:13:07 +0300

    disable corsconfig

--- src/main/java/swd4ta022/ticketguru/CorsConfiguration.java
+++ src/main/java/swd4ta022/ticketguru/CorsConfiguration.java
@@ -1,33 +1,33 @@
-package swd4ta022.ticketguru;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-
-@Configuration
-@EnableWebMvc
-public class CorsConfiguration implements WebMvcConfigurer{
-	
-	@Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedMethods("*");
-    }
-	
-   @Bean
-    public WebMvcConfigurer corsConfigurer(){
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                .allowedMethods("*")
-                .allowedOrigins("*")
-                .allowedHeaders("*")
-                .allowCredentials(false);
-            }
-        };
-    }
-}
+//package swd4ta022.ticketguru;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//
+//@Configuration
+//@EnableWebMvc
+//public class CorsConfiguration implements WebMvcConfigurer{
+//	
+//	@Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedMethods("*");
+//    }
+//	
+//   @Bean
+//    public WebMvcConfigurer corsConfigurer(){
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                .allowedMethods("*")
+//                .allowedOrigins("*")
+//                .allowedHeaders("*")
+//                .allowCredentials(false);
+//            }
+//        };
+//    }
+//}

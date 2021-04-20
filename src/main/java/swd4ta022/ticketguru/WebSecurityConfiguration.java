9fde3af404380490bbfd2a755110d77498d200f3
tuomas <mikko.martikainen3@myy.haaga-helia.fi>
2021-04-20 19:53:44 +0300

    cors200

--- src/main/java/swd4ta022/ticketguru/WebSecurityConfiguration.java
+++ src/main/java/swd4ta022/ticketguru/WebSecurityConfiguration.java
@@ -33,14 +33,14 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
-        	
+        	http.csrf().disable().cors();
 			http.authorizeRequests()
 			.antMatchers("/").permitAll()
 			.anyRequest()
 			.authenticated()
 			.and()
 			.httpBasic();
-			http.csrf().disable().cors();
+			
 			http.cors();
 	}
 

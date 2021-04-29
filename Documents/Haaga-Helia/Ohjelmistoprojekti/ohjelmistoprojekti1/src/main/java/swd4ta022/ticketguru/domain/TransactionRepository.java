952cf33f8a899054f594737f1c2cbcca20bbdd5c
niinakoo <64028007+niinakoo@users.noreply.github.com>
2021-04-29 17:40:21 +0300

    Delete Documents/Haaga-Helia/Ohjelmistoprojekti/ohjelmistoprojekti1 directory

--- Documents/Haaga-Helia/Ohjelmistoprojekti/ohjelmistoprojekti1/src/main/java/swd4ta022/ticketguru/domain/TransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package swd4ta022.ticketguru.domain;
-
-import org.springframework.data.repository.CrudRepository;
-import java.util.List;
-
-public interface TransactionRepository extends CrudRepository<Transaction, Long> {
-	List<Transaction> findByTrid(long trid);
-
-}

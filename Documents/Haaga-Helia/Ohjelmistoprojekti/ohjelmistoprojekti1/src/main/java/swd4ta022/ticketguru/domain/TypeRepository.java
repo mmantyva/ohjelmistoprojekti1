9e27172c7dddc775bf2b3b5611998b8b779732ca
niinakoo <64028007+niinakoo@users.noreply.github.com>
2021-05-03 18:22:46 +0300

    Delete Documents/Haaga-Helia/Ohjelmistoprojekti/ohjelmistoprojekti1 directory

--- Documents/Haaga-Helia/Ohjelmistoprojekti/ohjelmistoprojekti1/src/main/java/swd4ta022/ticketguru/domain/TypeRepository.java
+++
@@ -1,8 +0,0 @@
-package swd4ta022.ticketguru.domain;
-
-import java.util.List;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TypeRepository extends CrudRepository<Type, Long>{
-	List<Type> findByTypename(String typename);
-}
